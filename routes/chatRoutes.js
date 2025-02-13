cb87d78c8d2819ca15b07cfc84b2b24f840551a6
hoc vu <hocvu2003@gmail.com>
2025-02-13 11:45:19 +0700

    comment: tao comment

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/:userId/:contactId', chatController.getChatHistory);
 router.post('/send', chatController.saveMessage);
+// test tinh nang git rebase
 
 module.exports = router;
